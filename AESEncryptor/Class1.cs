644472f6cd1ab4d515a753cf599858003aeb95b7
Samuel Bancroft <samuelbancroft@hotmail.com>
2018-12-21 18:50:52 +0000

    entry point

--- AESEncryptor/Class1.cs
+++ AESEncryptor/Class1.cs
@@ -1,12 +1,98 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AESEncryptor
 {
-    public class Class1
+    public class Encryptor
     {
+        static byte[] EncryptStringToBytes(string plainText, byte[] Key, byte[] IV)
+        {
+            if (plainText == null || plainText.Length <= 0)
+            {
+                throw new ArgumentNullException(nameof(plainText));
+            }
+
+            if (Key == null || Key.Length <= 0)
+            {
+                throw new ArgumentNullException(nameof(Key));
+            }
+
+            if (IV == null || IV.Length <= 0)
+            {
+                throw new ArgumentNullException(nameof(IV));
+            }
+
+            byte[] encrypted;
+            using (var rijAlg = new RijndaelManaged())
+            {
+                rijAlg.Key = Key;
+                rijAlg.IV = IV;
+
+                var encryptor = rijAlg.CreateEncryptor(rijAlg.Key, rijAlg.IV);
+
+                using (var msEncrypt = new MemoryStream())
+                {
+                    using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (var swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(plainText);
+                        }
+
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+
+            return encrypted;
+
+        }
+
+        static string DecryptStringFromBytes(byte[] cipherText, byte[] Key, byte[] IV)
+        {
+            if (cipherText == null || cipherText.Length <= 0)
+            {
+                throw new ArgumentNullException(nameof(cipherText));
+            }
+
+            if (Key == null || Key.Length <= 0)
+            {
+                throw new ArgumentNullException(nameof(Key));
+            }
+
+            if (IV == null || IV.Length <= 0)
+            {
+                throw new ArgumentNullException(nameof(IV));
+            }
+
+            string plainText = null;
+
+            using (var rijAlg = new RijndaelManaged())
+            {
+                rijAlg.Key = Key;
+                rijAlg.IV = IV;
+
+                var decryptor = rijAlg.CreateDecryptor(rijAlg.Key, rijAlg.IV);
+
+                using (var msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (var srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            plainText = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+
+            return plainText;
+        }
     }
 }
